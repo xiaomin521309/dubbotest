40334eb6dca4c80404199f317d795bc472777017
xiaomin <781387150Qqq.com>
2017-07-20 11:19:08 +0800

    技术传播

--- dubboConsumer/src/test/java/consumer/hello/StartConsumerTest.java
+++ dubboConsumer/src/test/java/consumer/hello/StartConsumerTest.java
@@ -30,6 +30,7 @@ import static org.junit.Assert.*;
             System.out.println("有用有用");
             System.out.println("有用有用");
             System.out.println("有用有用");
+            System.out.println("1234567");
             String shiyan=helloService.sayHello();
             System.out.println(shiyan);
 
