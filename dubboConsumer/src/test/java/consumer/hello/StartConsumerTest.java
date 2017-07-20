f3ed3c7ff2f5bf91eb64ac0871d7b7f3224e759d
xiaomin <781387150Qqq.com>
2017-07-20 15:13:13 +0800

    真的是最后一次

--- dubboConsumer/src/test/java/consumer/hello/StartConsumerTest.java
+++ dubboConsumer/src/test/java/consumer/hello/StartConsumerTest.java
@@ -35,6 +35,7 @@ import static org.junit.Assert.*;
             System.out.println("52130987654321");
             System.out.println("爱党爱军爱人民");
             System.out.println("青龙白虎");
+            System.out.println("真的是最后一次");
             String shiyan=helloService.sayHello();
             System.out.println(shiyan);
 
