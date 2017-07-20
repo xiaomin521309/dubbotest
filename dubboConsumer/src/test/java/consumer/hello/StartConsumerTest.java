476931f051e1ed7861a5bce6657f47e995396802
xiaomin <781387150Qqq.com>
2017-07-20 11:42:18 +0800

    操作

--- dubboConsumer/src/test/java/consumer/hello/StartConsumerTest.java
+++ dubboConsumer/src/test/java/consumer/hello/StartConsumerTest.java
@@ -43,6 +43,7 @@ import static org.junit.Assert.*;
             System.out.println("有用有用");
             System.out.println("我才实操git");
             System.out.println("这是实际操作");
+            System.out.println("52130987654321");
             String shiyan=helloService.sayHello();
             System.out.println(shiyan);
 
