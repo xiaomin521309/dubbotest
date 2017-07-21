8eb56ab5341f37d28d54bc7eae770d6ca3c86700
xiaomin521309 <781387150@qq.com>
2017-07-21 11:42:30 +0800

    ...

--- dubboConsumer/src/test/java/consumer/hello/StartConsumerTest.java
+++ dubboConsumer/src/test/java/consumer/hello/StartConsumerTest.java
@@ -43,6 +43,7 @@ import static org.junit.Assert.*;
             System.out.println("吃葡萄不吐葡萄皮儿吗");
             System.out.println("吃葡萄不吐葡萄皮儿吗");
             System.out.println("变化");
+            System.out.println("变化");
             String shiyan=helloService.sayHello();
             System.out.println(shiyan);
 
